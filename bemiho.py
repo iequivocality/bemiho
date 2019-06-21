8ac4781797862f3e46f03c17aa0c23b40402cb04
iequivocality <xanthiandere@gmail.com>
2019-06-22 00:37:21 +0800

    refactor args and dry implementation done

--- bemiho.py
+++ bemiho.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from extractor.json_extractor import JSONExtractor
 from extractor.json_mapper import GroupJSONObjectMapper, MemberJSONObjectMapper
-from args import get_user_input
+from input import get_user_input
 
 user_input = get_user_input()
 print(user_input)
---
+++ input.py
@@ -0,0 +1,91 @@
+import argparse
+
+from extractor.json_extractor import JSONExtractor
+from extractor.json_mapper import GroupJSONObjectMapper, MemberJSONObjectMapper
+from user_input import BemihoUserInput, BemihoUserInputBuilder
+
+from args import parse_system_args
+from const import CONTENT_CHOICES
+
+def group_format(index, group):
+    return f"({index + 1}) {group.romaji} - {group.kanji}"
+
+def member_format(index, member):
+    return f"({index + 1}) {member.romaji} - {member.kanji}"
+
+def ask_for_user_input_with_extracted(options, format, label):
+    print(f"Available {label}s:")
+    for index, option_item in enumerate(options):
+        print(format(index, option_item))
+    return input(f"Please select {label}: ")
+
+def get_input_and_check_index(json_file, json_mapper, value_from_args, input_req_format, input_req_label, predicate_for_json_data):
+    extractor = JSONExtractor(json_file, json_mapper())
+    extracted_data = extractor.extract()
+
+    input_value = None
+    if (value_from_args == None):
+        input_value = ask_for_user_input_with_extracted(extracted_data, input_req_format, input_req_label)
+    else:
+        input_value = value_from_args
+
+    selected_group = next((datum for datum in extracted_data if predicate_for_json_data), None)
+    if (selected_group == None):
+        # should raise exception instead of calling quit
+        print("Group not found on Bemiho's index.")
+        quit()
+    return selected_group
+
+def get_group_input(group_from_args):
+    def data_check(group):
+        return group.kanji == group_input or group.romaji == group_input or group.code == group_input
+    return get_input_and_check_index('index/index.idols', GroupJSONObjectMapper, group_from_args, group_format, "group", data_check)
+
+def get_member_input(member_from_args, selected_group):
+    def data_check(member):
+        return member.kanji == member_input or member.romaji == member_input or member.kana == member_input
+    return get_input_and_check_index('index/' + selected_group.index, MemberJSONObjectMapper, member_from_args, member_format, "member", data_check)
+
+def get_page_input(page_from_args, default_value, label):
+    if (page_from_args == None):
+        page_input = input(f"Select {label}. Default is {default_value}: ")
+        if (page_input == None or page_input == ''):
+            page_input = default_value
+        if (not page_input.isdigit()):
+            print("Page numbers have to be digits.")
+            quit()
+    else:
+        page_input = page_from_args
+    return page_input
+
+def get_user_input():
+    args = parse_system_args()
+    user_input_build = BemihoUserInputBuilder()
+
+    selected_group = get_group_input(args.group)
+    user_input_build.set_group(selected_group)
+    user_input_build.set_member(get_member_input(args.member, selected_group))
+
+    selected_content = None
+    if (args.content == None):
+        selected_content = input(f"Please select content to pull. Possible choices are ({', '.join(CONTENT_CHOICES)}). Default is photos: ")
+        if (selected_content == None or selected_content == ''):
+            selected_content = 'photos'
+    else:
+        selected_content = args.content
+
+    if (any(selected_content == choice for choice in CONTENT_CHOICES)):
+        user_input_build.set_content(args.content)
+    else:
+        print("Invalid content set by the user.")
+        quit()
+
+    firstpage = int(get_page_input(args.firstpage, '1', "first page"))
+    lastpage = int(get_page_input(args.lastpage, '1', "last page"))
+    if (firstpage > lastpage):
+        print("First page cannot be larger than the last page.")
+        quit()
+    else:
+        user_input_build.set_firstpage(firstpage)
+        user_input_build.set_lastpage(lastpage)
+    return user_input_build.build()
