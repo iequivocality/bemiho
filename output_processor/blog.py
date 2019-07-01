d3cb84949e1eaab67e633015c24dde9454700f6d
iequivocality <xanthiandere@gmail.com>
2019-07-02 06:32:50 +0800

    save to docx file success, refactor and change to better font next

--- output_processor/blog.py
+++ output_processor/blog.py
@@ -1,31 +1,35 @@
 from os.path import join
 
-from fpdf import FPDF
+import io
+import requests
 
 from output_processor import ScrapperOutputProcessor
+from contents import BlogTextContent, BlogImageContent
+
+from docx import Document
+from docx.shared import Inches
 
 class BlogEntryOutputProcessor(ScrapperOutputProcessor):
     content = 'blog'
-    def build_pdf(self, directory, blog_data):
+    def build_document(self, directory, blog_data):
         header = blog_data.header
         contents = blog_data.contents
-        
-        pdf_path = join(directory, f"{header.title}.pdf")
-        pdf = FPDF(orientation='P', unit='mm', format='A4')
-        pdf.add_page()
 
-        pdf.add_font('fireflysung', '', 'fireflysung.ttf', uni=True) 
-        pdf.set_font('fireflysung', '', 14)
+        document_path = join(directory, f"{header.title}.docx")
+        document = Document()
+        style = document.styles['Normal']
         
         for content in contents:
-            print(content)
-            # pdf.write(8, content.get_content())
-            # pdf.ln(8)
-            # pdf.cell(0, 0, txt='TEST', ln=1)
-        
-        pdf.output(pdf_path)
+            if (isinstance(content, BlogTextContent)):
+                # print("text")
+                document.add_paragraph(content.get_content())
+            elif (isinstance(content, BlogImageContent)):
+                response = requests.get(content.get_content(), stream=True)
+                image = io.BytesIO(response.content)
+                document.add_picture(image, width=Inches(6))
+        document.save(document_path)
 
     def process_blog_data(self, blog_datas):
         directory = self.output_folder_handler.get_directory_for_member_subdirectories(self.content)
         for blog_data in blog_datas:
-            self.build_pdf(directory, blog_data)
+            self.build_document(directory, blog_data)
