9373137827b724763ee681e2395d7fdf2b065e29
iequivocality <xanthiandere@gmail.com>
2019-08-10 11:40:54 +0800

    initial nijicon

---
+++ scrapper/nijicon.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+from scrapper import Scrapper
+from logger import BemihoLogger
+from contents import BlogHeader, BlogData
+from services.lineblog import LineBlogApiCrawler
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from services.lineblog import LineBlogService
+
+class NijiconScrapper(Scrapper):
+    code = 'Nijicon'
+    def __init__(self, user_input, page_number, traversal):
+        super().__init__(user_input, page_number, traversal)
+        self.logger = BemihoLogger(self.__class__).get_logger()
+
+    @staticmethod
+    def get_proper_page_index(page_number):
+        return page_number - 1
+
+    def format_url(self, page_number):
+        member = self.user_input.member
+        group = self.user_input.group
+        return f"{member.blog}&{group.pageformat}{page_number}"
+    
+    def start_web_scrape(self):
+        url = self.format_url(self.page_number)
+        services = LineBlogService(url, self.page_number, self.user_input.member.kanji, self.traversal)
+        contents = services.serve_contents()
+        return contents
